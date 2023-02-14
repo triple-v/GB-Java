080d9e9a4ca0181bece97602c32dd4c52a518c2e
triple-v <vasilevich.account@ya.ru>
2023-02-15 01:58:52 +0300

    Второе задание (Семинар 1)

---
+++ Task_2.java
@@ -0,0 +1,12 @@
+public class Task_2 {
+  public static void main(String[] args) {
+    System.out.println(isPositive(13));
+  }
+
+  private static boolean isPositive(int number) {
+    if (number >= 0) {
+      return true;
+    } else
+      return false;
+  }
+}
